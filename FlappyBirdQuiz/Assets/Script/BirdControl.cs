63707b83c4a29dad9947edca42e88088cf927d3c
Sharon Oh <ohs790@newschool.edu>
2018-10-18 18:42:08 -0400

    initial commit

---
+++ FlappyBirdQuiz/Assets/Script/BirdControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdControl : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float flapForce = 6f;
+    public Text gameover;
+    public int Points;
+    public Text pointsText;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        Points = 0;
+        pointsText.text = "SCORE :" + Points.ToString();
+        gameover.text = "";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+            rb.velocity = new Vector2(5, flapForce);
+            Points = Points + 1;
+        }
+
+	}
+
+    void OnTriggerEnter2D(Collider hit)
+    {
+        if (hit.gameObject.CompareTag ("Score"))
+        {
+            Points++;
+
+            pointsText.text = "SCORE :" + Points.ToString();
+
+
+
+        }
+    }
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (hit.gameObject.CompareTag ("Ground") || hit.gameObject.CompareTag ("Obs"))
+        {
+            gameover.text = "GAME OVER";
+        }
+
+    }
+}
---
+++ FlappyBirdQuiz/Assets/Script/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    private GameObject Bird;
+
+    private float pX;
+
+
+    private Vector2 currentVelocity;
+    public float smoothX;
+    public float smoothY;
+
+
+    void Awake()
+    {
+        Bird = GameObject.FindGameObjectWithTag("Bird");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Bird == null)
+        {
+            Bird = GameObject.FindGameObjectWithTag("Bird");
+        }
+        else
+        {
+            SmoothCamera();
+        }
+    }
+
+
+    void SmoothCamera()
+    {
+        pX = Mathf.SmoothDamp(transform.position.x, Bird.transform.position.x, ref currentVelocity.x, smoothX);
+       
+        transform.position = new Vector3(pX, transform.position.y, transform.position.z);
+    }
+}
+
+
+
+
